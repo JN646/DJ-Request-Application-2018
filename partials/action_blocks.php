d72adae2d8c0a025a90ca48803b316ceb40378c2
JN646 <josh.ginn@hotmail.co.uk>
2018-05-23 22:27:48 +0100

    Change Action Blocks

--- partials/action_blocks.php
+++ partials/action_blocks.php
@@ -1,15 +1,6 @@
+<!-- Block 1 -->
 <div class="card collection_block">
   <div class="card-body">
     <a href="<?php echo $environment; ?>admin/add_song.php"><p class='card-text'>Add Song</p></a>
-  </div>
-</div>
-<div class="card collection_block">
-  <div class="card-body">
-    <a href="#"><p class='card-text'>Action 2</p></a>
-  </div>
-</div>
-<div class="card collection_block">
-  <div class="card-body">
-    <a href="#"><p class='card-text'>Action 3</p></a>
   </div>
 </div>
