e6c7a9a8462a52816f024917597425dfa4d20a4e
JN646 <josh.ginn@hotmail.co.uk>
2018-05-24 22:46:24 +0100

    Included ability to turn front application on and off.

--- index.php
+++ index.php
@@ -1,26 +1,40 @@
 <?php
   // Include Header
   include 'partials/_header.php';
+
+  // App Running Debug.
+  $appRunning = 1;
 ?>
       <!-- Row -->
       <div class="row">
 
         <!-- Small Side -->
         <div id='collectionWin' class="col-md-2">
+
           <?php
+          // If Application is Running.
+          if ($appRunning == 1) {
             // Include Blocks
             include 'partials/collection_blocks.php';
+          }
           ?>
+
         </div>
 
         <!-- Main Window -->
         <div id='mainWin' class="col-md-10">
 
+          <!-- Row -->
           <div class="row">
+
             <?php
+            // If Application is Running.
+            if ($appRunning == 1) {
               // Include Blocks
               include 'partials/song_blocks.php';
+            }
             ?>
+
           </div>
 
         </div>
