b5b6521beb4d2abce34f3032ab633dfa4ad80de1
JN646 <josh.ginn@hotmail.co.uk>
2018-05-25 19:51:29 +0100

    Time Simplifier. Displays time song requested in a nicer format.

--- functions/functions.php
+++ functions/functions.php
@@ -129,17 +129,20 @@ function RequestList($mysqli) {
                   <th class='text-center'>Song Name</th>
                   <th class='text-center'>Song Artist</th>
                   <th class='text-center'>Song Album</th>
+                  <th class='text-center'>Time</th>
                   <th class='text-center'>Clear</th>
               </tr>
       <?php
           while($row = mysqli_fetch_array($result)){
             $RequestID = $row['request_id'];
+            $Time = $row['request_time'];
 
               echo "<tr>";
                   echo "<td class='text-center'>" . $row['request_id'] . "</td>";
                   echo "<td class='text-center'>" . $row['request_s_name'] . "</td>";
                   echo "<td class='text-center'>" . $row['request_s_artist'] . "</td>";
                   echo "<td class='text-center'>" . $row['request_s_album'] . "</td>";
+                  echo "<td class='text-center'>" . nicetime($Time) . "</td>";
                   echo "<td class='text-center'><a href='". $_SERVER['PHP_SELF'] . "?" . $RequestID . "'>Clear</a></td>";
               echo "</tr>";
           }
@@ -159,6 +162,46 @@ function RequestList($mysqli) {
   if (isset($_GET[$RequestID])) {
     echo "The record " . $RequestID . " Made Inactive";
   }
+}
+
+function nicetime($date)
+{
+    if(empty($date)) {
+        return "No date provided";
+    }
+
+    $periods         = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+    $lengths         = array("60","60","24","7","4.35","12","10");
+
+    $now             = time();
+    $unix_date         = strtotime($date);
+
+       // check validity of date
+    if(empty($unix_date)) {
+        return "Bad date";
+    }
+
+    // is it future date or past date
+    if($now > $unix_date) {
+        $difference     = $now - $unix_date;
+        $tense         = "ago";
+
+    } else {
+        $difference     = $unix_date - $now;
+        $tense         = "from now";
+    }
+
+    for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+        $difference /= $lengths[$j];
+    }
+
+    $difference = round($difference);
+
+    if($difference != 1) {
+        $periods[$j].= "s";
+    }
+
+    return "$difference $periods[$j] {$tense}";
 }
 
 // GLOBAL FUNCTIONS
