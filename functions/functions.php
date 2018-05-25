6445c5d2371e37044eb5e9812404d517599fcd83
JN646 <josh.ginn@hotmail.co.uk>
2018-05-25 15:31:12 +0100

    List all songs moved to functions.php. Genre colour fix.

--- functions/functions.php
+++ functions/functions.php
@@ -50,4 +50,46 @@ function GenreArray() {
     echo "<option value='" . $genreList[$i] ."'>" . $genreList[$i] ."</option>";
   }
 }
+
+function ListSongs($mysqli) {
+  // Attempt select query execution
+  $sql = "SELECT * FROM songs ORDER BY song_name ASC";
+  if($result = mysqli_query($mysqli, $sql)){
+      if(mysqli_num_rows($result) > 0){
+        ?>
+          <table id='table_search' class='table table-bordered'>
+              <tr>
+                  <th class='text-center'>ID</th>
+                  <th class='text-center'>Song Name</th>
+                  <th class='text-center'>Song Artist</th>
+                  <th class='text-center'>Song Album</th>
+                  <th class='text-center'>Song Genre</th>
+              </tr>
+      <?php
+          while($row = mysqli_fetch_array($result)){
+              echo "<tr>";
+                  echo "<td class='text-center'>" . $row['song_id'] . "</td>";
+                  echo "<td>" . $row['song_name'] . "</td>";
+                  echo "<td>" . $row['song_artist'] . "</td>";
+                  echo "<td>" . $row['song_album'] . "</td>";
+                  echo "<td class='colourCell" . $row['song_genre'] . "'>" . $row['song_genre'] . "</td>";
+              echo "</tr>";
+          }
+          echo "</table>";
+
+          // Free result set
+          mysqli_free_result($result);
+      } else{
+
+          // Error Message
+          echo "<p>No songs were found.</p>";
+      }
+  } else{
+      // SQL Error
+      echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+  }
+
+  // Close connection
+  mysqli_close($mysqli);
+}
  ?>
--- partials/feed_blocks.php
+++ partials/feed_blocks.php
@@ -1,21 +1,47 @@
-
 <div class="col-md-12">
   <div class="">
     <?php
-    $sql = "SELECT * FROM requests WHERE request_active = '1'";
-    $result = mysqli_query($mysqli, $sql);
+    // Attempt select query execution
+    $sql = "SELECT * FROM requests WHERE request_active = 1 ORDER BY request_time ASC";
+    if($result = mysqli_query($mysqli, $sql)){
+        if(mysqli_num_rows($result) > 0){
+          ?>
+            <table id='table_search' class='table table-bordered'>
+                <tr>
+                    <th class='text-center'>ID</th>
+                    <th class='text-center'>Song Name</th>
+                    <th class='text-center'>Song Artist</th>
+                    <th class='text-center'>Song Album</th>
+                    <th class='text-center'>Clear</th>
+                </tr>
+        <?php
+            while($row = mysqli_fetch_array($result)){
+              $RequestID = $row['request_id'];
 
-    if (mysqli_num_rows($result) > 0) {
-
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            echo $row["request_s_name"]. " - " . $row["request_s_artist"]. "<br>";
+                echo "<tr>";
+                    echo "<td class='text-center'>" . $row['request_id'] . "</td>";
+                    echo "<td>" . $row['request_s_name'] . "</td>";
+                    echo "<td>" . $row['request_s_artist'] . "</td>";
+                    echo "<td>" . $row['request_s_album'] . "</td>";
+                    echo "<td><a href='". $_SERVER['PHP_SELF'] . "?" . $RequestID . "'>Clear</a></td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            // Free result set
+            mysqli_free_result($result);
+        } else{
+            echo "No active requests were found.";
         }
-    } else {
-        echo "0 results";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
     }
 
+    // Close connection
     mysqli_close($mysqli);
+
+    if (isset($_GET[$RequestID])) {
+      echo "The record " . $RequestID . " Made Inactive";
+    }
     ?>
   </div>
 </div>
