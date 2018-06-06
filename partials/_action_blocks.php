2f4fbdd31df7f3a1c9767cb58b7441e401c4fc42
JN646 <josh.ginn@hotmail.co.uk>
2018-06-06 17:31:31 +0100

    Change Action Block Icons.

--- partials/_action_blocks.php
+++ partials/_action_blocks.php
@@ -5,8 +5,8 @@
       <h5>Actions</h5>
     </div>
     <ul class="nav flex-column">
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/index.php"><i class="fas fa-music"></i> Requests</a></li>
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/list_manager.php"><i class="fas fa-music"></i> Song Control</a></li>
+      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/index.php"><i class="fas fa-list"></i> Requests</a></li>
+      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/list_manager.php"><i class="fas fa-tachometer-alt"></i> Song Control</a></li>
     </ul>
   </div>
 </div>
@@ -22,7 +22,7 @@
         <span><a class="nav-link-small font-button plus" style="cursor: pointer;">A+</a></span>
         <span><a class="nav-link-small font-button minus" style="cursor: pointer;">A-</a></span>
       </li>
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>songs/index.php"><i class="fas fa-music"></i> Coming Soon</a></li>
+      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>songs/index.php"><i class="fas fa-download"></i> Export Song List</a></li>
     </ul>
   </div>
 </div>
