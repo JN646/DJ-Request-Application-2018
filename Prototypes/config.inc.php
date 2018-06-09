dee7fef8fac687f2fb723a2fe610ab91cdf6e32c
JN646 <josh.ginn@hotmail.co.uk>
2018-06-09 15:10:59 +0100

    Pagination Slow Integration.

--- Prototypes/config.inc.php
+++ Prototypes/config.inc.php
@@ -1,12 +1,10 @@
 <?php
-/* Title : Ajax Pagination with jQuery & PHP
-Example URL : http://www.sanwebe.com/2013/03/ajax-pagination-with-jquery-php */
 
 $db_username 		= 'root'; //database username
 $db_password 		= ''; //dataabse password
 $db_name 			= 'djapp2'; //database name
 $db_host 			= 'localhost'; //hostname or IP
-$item_per_page 		= 5; //item to display per page
+$item_per_page 		= 15; //item to display per page
 
 $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
 //Output any connection error
