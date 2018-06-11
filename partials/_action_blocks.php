cbd8cbcb7aab676184ffec5ebcd3bd6ec7027323
JN646 <josh.ginn@hotmail.co.uk>
2018-06-11 21:33:07 +0100

    Action Block Redesign.

--- partials/_action_blocks.php
+++ partials/_action_blocks.php
@@ -4,27 +4,27 @@
     <div class="card-header text-center">
       <h5>Actions</h5>
     </div>
-    <ul class="nav flex-column">
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/stats.php"><i class="fas fa-trophy"></i> Stats</a></li>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"><a class="" href="<?php echo $environment; ?>admin/stats.php"><i class="fas fa-trophy"></i> Stats</a></li>
     </ul>
   </div>
 </div>
 
-<!-- Views -->
+<!-- Songs -->
 <div class="card action_block">
   <div class="card">
     <div class="card-header text-center">
       <h5>Songs</h5>
     </div>
-    <ul class="nav flex-column">
-      <li>
-        <span><a class="nav-link-small font-button plus" style="cursor: pointer;"><i class="fas fa-search-plus"></i></a></span>
-        <span><a class="nav-link-small font-button minus" style="cursor: pointer;"><i class="fas fa-search-minus"></i></a></span>
+    <ul class="list-group list-group-flush">
+      <li class='list-group-item'>
+        <span><a class="font-button plus" style="cursor: pointer;"><i class="fas fa-search-plus"></i></a></span>
+        <span><a class="font-button minus" style="cursor: pointer;"><i class="fas fa-search-minus"></i></a></span>
       </li>
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/index.php"><i class="fas fa-list"></i> Requests <span class="badge badge-secondary"><?php echo countRequestsActive($db); ?></span></a></li>
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/list_manager.php"><i class="fas fa-tachometer-alt"></i> Song Control</a></li>
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>functions/exportData.php"><i class="fas fa-download"></i> Export Song List</a></li>
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/index.php?deleterequests=true"><i class="fas fa-trash"></i> Delete All Requests</a></li>
+      <li class="list-group-item"><a class="" href="<?php echo $environment; ?>admin/index.php"><i class="fas fa-list"></i> Requests <span class="badge badge-secondary"><?php echo countRequestsActive($db); ?></span></a></li>
+      <li class="list-group-item"><a class="" href="<?php echo $environment; ?>admin/list_manager.php"><i class="fas fa-tachometer-alt"></i> Song Control</a></li>
+      <li class="list-group-item"><a class="" href="<?php echo $environment; ?>functions/exportData.php"><i class="fas fa-download"></i> Export Song List</a></li>
+      <li class="list-group-item"><a class="text-danger" href="<?php echo $environment; ?>admin/index.php?deleterequests=true"><i class="fas fa-trash"></i> Delete All Requests</a></li>
     </ul>
   </div>
 </div>
@@ -36,8 +36,8 @@
     <div class="card-header text-center">
       <h5>Drinks</h5>
     </div>
-    <ul class="nav flex-column">
-      <li class="nav-item"><a class="nav-link" href="<?php echo $environment; ?>admin/drink_manager.php"><i class="fas fa-beer"></i> Drink Manager</a></li>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"><a class="" href="<?php echo $environment; ?>admin/drink_manager.php"><i class="fas fa-beer"></i> Drink Manager</a></li>
     </ul>
   </div>
 </div>
