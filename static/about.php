fda343899b2f56b5341767c641dbbce9fa5cd508
JN646 <josh.ginn@hotmail.co.uk>
2018-06-19 17:54:41 +0100

    Added stati page classes and custom css. Added about page.

---
+++ static/about.php
@@ -0,0 +1,22 @@
+<?php include '../partials/_header.php'; ?>
+
+      <?php
+      // Start session if one not running.
+      if(!isset($_SESSION)){
+         session_start();
+      }
+      ?>
+
+        <!-- Main Window -->
+        <div class="container-fluid">
+          <div id='staticWin' class="col-md-12">
+              <h1 class='display-4'>About</h1>
+              <h2>Getting Started</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <br>
+              <h2>Requesting Songs</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+<?php include '../partials/_footer.php'; ?>
