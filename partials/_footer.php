dd0c2c1369f02cdc488b2874a5c5b2ba105131d2
JN646 <josh.ginn@hotmail.co.uk>
2018-06-19 22:19:21 +0100

    Footer links and privacy page.

--- partials/_footer.php
+++ partials/_footer.php
@@ -3,6 +3,8 @@
       <div id='footerBar' class="col-md-12">
         <p class='footerLink'><a href="<?php echo $environment; ?>admin/index.php"><i class="fas fa-unlock-alt fa-2x"></i></a></p>
         <p class='footerLink'><a href="<?php echo $environment; ?>index.php"><i class="fas fa-home fa-2x"></i></a></p>
+        <p class='footerLink'><a href="<?php echo $environment; ?>static/about.php">About</a></p>
+        <p class='footerLink'><a href="<?php echo $environment; ?>static/privacy.php">Privacy Policy</a></p>
       </div>
     </div>
   </div>
