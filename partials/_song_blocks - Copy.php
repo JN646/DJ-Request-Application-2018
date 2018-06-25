d8cde5671a93d5306c48d692334ebb80b01f6e34
JN646 <josh.ginn@hotmail.co.uk>
2018-06-25 10:07:01 +0100

    Removed Pagination, backup old.

--- partials/_song_blocks - Copy.php
+++ partials/_song_blocks - Copy.php
@@ -13,19 +13,45 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/dj-app2/functions/functions.php");
 require_once($_SERVER["DOCUMENT_ROOT"] . "/dj-app2/lib/lastfm.php");
 
 // Variables
-// $collectionIDNum = 0;
-$coverArtMode = 0;
+$collectionIDNum = 0;
 $orderType = 'artist';
 
+//continue only if $_POST is set and it is a Ajax request
+if (isset($_POST) && isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/dj-app2/config/DBConfig.php");
+    //Get page number from Ajax POST
+    if (isset($_POST["page"])) {
+        $page_number = filter_var($_POST["page"], FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_STRIP_HIGH); //filter number
+        if (!is_numeric($page_number)) {
+            die('Invalid page number!');
+        } //incase of invalid page number
+    } else {
+        $page_number = 1; //if there's no page number, set it to 1
+    }
+
     //get total number of records from database for pagination
-    $songblock_sql = mysqli_query($db, "SELECT * FROM crud WHERE collec_id = $collectionIDNum ORDER BY $orderType ASC LIMIT 20");
+    $results = $mysqli->query("SELECT COUNT(*) FROM crud WHERE collec_id = $collectionIDNum");
+    $get_total_rows = $results->fetch_row(); //hold total records in variable
+    //break records into pages
+    $total_pages = ceil($get_total_rows[0]/$item_per_page);
+
+    //get starting position to fetch the records
+    $page_position = (($page_number-1) * $item_per_page);
+
+    //Limit our results within a specified range.
+    $results = $mysqli->prepare("SELECT id, name, artist, album, genre, collec_id FROM crud WHERE collec_id = $collectionIDNum ORDER BY $orderType ASC LIMIT $page_position, $item_per_page");
+    $results->execute(); //Execute prepared Query
+    $results->bind_result($id, $SongName, $SongArtist, $SongAlbum, $SongGenre, $collec_id); //bind variables to prepared statement
+
+    // Pagination Link Block
+    echo "<div class='row paginateButtons'>";
+      echo paginate_function($item_per_page, $page_number, $get_total_rows[0], $total_pages);
+    echo '</div>';
+
+    echo "</br>";
 
     //Display records fetched from database.
-    while ($row = mysqli_fetch_array($songblock_sql)) {
-            $SongGenre = $row['genre'];
-            $SongName = $row['name'];
-            $SongArtist = $row['artist'];
-            $SongAlbum = $row['album'];
+    while ($results->fetch()) { //fetch values
       ?>
               <!-- Song Blocks -->
               <div class="col-sm-4 col-md-2 card song_block <?php echo 'colour' . $SongGenre ?>" style="padding-left: 0px; padding-right: 0px;">
@@ -59,7 +85,11 @@ $orderType = 'artist';
                   </div>
                 </div>
               </div>
-            </div>
               <?php
     }
+
+    echo "</div>";
+
+    exit;
+}
 ?>
