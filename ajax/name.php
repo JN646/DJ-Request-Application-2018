6ec487bcb613db84d2a1589167e982c3901e03e0
JN646 <josh.ginn@hotmail.co.uk>
2018-07-01 17:28:28 +0100

    Comments and fixed request function.

--- ajax/name.php
+++ ajax/name.php
@@ -1,14 +1,21 @@
 <?php
 if (isset($_POST['name']) === true && empty($_POST['name']) === false) {
 
+// Connect to database
 require_once '../config/DBConfig.php';
 
+  // Trim input.
   $name = trim($_POST['name']);
 
+  // Run Query.
   $songblock_sql = mysqli_query($mysqli, "SELECT * FROM crud WHERE name LIKE '%$name%' OR artist LIKE '%$name%' OR album LIKE '%$name%'");
+  // Catch any errors.
   mysqli_error($mysqli);
 
+  // Create blocks
   while ($row = mysqli_fetch_array($songblock_sql)) {
+      // Assign Variables
+      $SongID = $row['id'];
       $SongGenre = $row['genre'];
       $SongName = $row['name'];
       $SongArtist = $row['artist'];
@@ -32,7 +39,7 @@ require_once '../config/DBConfig.php';
 
             <!-- Request Button -->
             <div id='requestButtonDiv' class="col-md-3">
-              <a href="index.php?request_song=<?php echo $id ?>"><i class="far fa-thumbs-up fa-2x"></i></a>
+              <a href="index.php?request_song=<?php echo $SongID ?>"><i class="far fa-thumbs-up fa-2x"></i></a>
             </div>
 
           </div>
